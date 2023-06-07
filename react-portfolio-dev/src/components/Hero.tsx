ae9c1001a5a37738db4c72b50d7a9e7e0d86bce8
Marcio Tanan <marcio-tanan@hotmail.com>
2023-06-07 11:50:32 -0300

    update session hero

--- react-portfolio-dev/src/components/Hero.tsx
+++ react-portfolio-dev/src/components/Hero.tsx
@@ -1,7 +1,29 @@
 export function Hero() {
   return(
-    <>
-    <h2 className="text-4xl underline font-bold">Hero test</h2>
-   </>
+    <section className="bg-gradient-to-tr from-black to-gray-900 text-white">
+      <div
+       className="container mx-auto max-w-4xl">
+        <h1 className="mb-6">
+          <span  className="font-handwriting block text-3xl"> Olá, me chamo</span>
+          <span className="font-headline text-5xl font-semibold">Márcio</span>
+          <span className="font-headline text-5xl font-light text-blue-400"> Tanan</span>
+        </h1>
+
+        <h2 className="font-semibold flex items-center gap-4">
+          <div className="h-1 w-20 rounded-full bg-blue-800"></div>
+          Desenvolvedor  Full-Stack
+        </h2>
+
+        <p className="text-gray-600">
+        Sou um desenvolvedor que ama criar experiências interativas e responsivas. Atualmente trabalho como freelancer e transformo ideias em sites e aplicações web.
+        </p>
+
+        <div>
+          
+        </div>
+    
+      </div>
+    
+   </section>
   )
 } 
