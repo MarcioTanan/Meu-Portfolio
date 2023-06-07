867d40c306ee60120f84a11a326122eac01eb6d4
Marcio Tanan <marcio-tanan@hotmail.com>
2023-06-07 12:28:19 -0300

    update hero

--- react-portfolio-dev/src/components/Hero.tsx
+++ react-portfolio-dev/src/components/Hero.tsx
@@ -1,10 +1,14 @@
+import {HiDownload} from "react-icons/hi"
+
 export function Hero() {
   return(
     <section className="bg-gradient-to-tr from-black to-gray-900 text-white">
       <div
-       className="container mx-auto max-w-4xl">
-        <h1 className="mb-6">
-          <span  className="font-handwriting block text-3xl"> Olá, me chamo</span>
+       className="container mx-auto max-w-4xl p-3 py-12 flex">
+
+         <div className="basis-1/2">
+           <h1 className="mb-6">
+          <span  className="font-handwriting block text-4xl text-center"> Olá, me chamo</span>
           <span className="font-headline text-5xl font-semibold">Márcio</span>
           <span className="font-headline text-5xl font-light text-blue-400"> Tanan</span>
         </h1>
@@ -14,14 +18,21 @@ export function Hero() {
           Desenvolvedor  Full-Stack
         </h2>
 
-        <p className="text-gray-600">
+        <p className="text-gray-600 my-6">
         Sou um desenvolvedor que ama criar experiências interativas e responsivas. Atualmente trabalho como freelancer e transformo ideias em sites e aplicações web.
         </p>
 
-        <div>
-          
+        <div className="flex items-center gap-2">
+          <a href="#" className="underline font-bold text-white">Fale Comigo
+          </a>
+          <span className="italic text-gray-500"> ou </span>
+          <a 
+          href="#" 
+          className="button flex items-center gap-2 text-gray-600 hover:text-gray-900"> <HiDownload/> Baixe meu CV
+          </a>
         </div>
-    
+        </div>
+       <div className="basis-1/2"></div>
       </div>
     
    </section>
